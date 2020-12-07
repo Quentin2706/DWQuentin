da5f261ad9f2f7e4402a34c7ff076331cb0bb33f
59011-95-02 <59011-95-02@exchange.ad.afpanet>
2020-12-07 17:20:02 +0100

    07/12/2020 17:20:02,32

---
+++ BackupBDD/20201207/backup_baseproduits_20201207.sql
@@ -0,0 +1,301 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: baseproduits
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `baseproduits`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `baseproduits` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `baseproduits`;
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `article` (
+  `idArticle` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleArticle` varchar(50) NOT NULL,
+  `prixHt` float NOT NULL,
+  `codeBarre` varchar(50) NOT NULL,
+  `idTva` int(11) NOT NULL,
+  `idCategorie` int(11) NOT NULL,
+  PRIMARY KEY (`idArticle`),
+  KEY `fk_article_tva` (`idTva`),
+  KEY `fk_article_categorie` (`idCategorie`),
+  CONSTRAINT `fk_article_categorie` FOREIGN KEY (`idCategorie`) REFERENCES `categories` (`idCategorie`),
+  CONSTRAINT `fk_article_tva` FOREIGN KEY (`idTva`) REFERENCES `tva` (`idTva`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `article`
+--
+
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `caisse`
+--
+
+DROP TABLE IF EXISTS `caisse`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `caisse` (
+  `idCaisse` int(11) NOT NULL AUTO_INCREMENT,
+  `nomCaisse` varchar(50) NOT NULL,
+  `totalCaisse` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `idUser` int(11) NOT NULL,
+  PRIMARY KEY (`idCaisse`),
+  KEY `fk_caisse_user` (`idUser`),
+  CONSTRAINT `fk_caisse_user` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `caisse`
+--
+
+LOCK TABLES `caisse` WRITE;
+/*!40000 ALTER TABLE `caisse` DISABLE KEYS */;
+/*!40000 ALTER TABLE `caisse` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `idCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleCategorie` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (3,'cat1');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ligneticket`
+--
+
+DROP TABLE IF EXISTS `ligneticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ligneticket` (
+  `idLigneTicket` int(11) NOT NULL AUTO_INCREMENT,
+  `quantite` int(11) NOT NULL,
+  `prixHt` float NOT NULL,
+  `montantTva` float NOT NULL,
+  `idTicket` int(11) NOT NULL,
+  `idArticle` int(11) NOT NULL,
+  PRIMARY KEY (`idLigneTicket`),
+  KEY `fk_ligneTicket_ticket` (`idTicket`),
+  KEY `fk_ligneTicket_article` (`idArticle`),
+  CONSTRAINT `fk_ligneTicket_article` FOREIGN KEY (`idArticle`) REFERENCES `article` (`idArticle`),
+  CONSTRAINT `fk_ligneTicket_ticket` FOREIGN KEY (`idTicket`) REFERENCES `ticket` (`idTicket`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ligneticket`
+--
+
+LOCK TABLES `ligneticket` WRITE;
+/*!40000 ALTER TABLE `ligneticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ligneticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `modepaiement`
+--
+
+DROP TABLE IF EXISTS `modepaiement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `modepaiement` (
+  `idModePaiement` int(11) NOT NULL AUTO_INCREMENT,
+  `typePaiement` varchar(50) NOT NULL,
+  PRIMARY KEY (`idModePaiement`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `modepaiement`
+--
+
+LOCK TABLES `modepaiement` WRITE;
+/*!40000 ALTER TABLE `modepaiement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modepaiement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paiement`
+--
+
+DROP TABLE IF EXISTS `paiement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paiement` (
+  `idPaiement` int(11) NOT NULL AUTO_INCREMENT,
+  `idModePaiement` int(11) NOT NULL,
+  `idTicket` int(11) NOT NULL,
+  `prixTTC` float NOT NULL,
+  PRIMARY KEY (`idPaiement`),
+  KEY `fk_paiement_ModePaiement` (`idModePaiement`),
+  KEY `fk_paiement_Ticket` (`idTicket`),
+  CONSTRAINT `fk_paiement_ModePaiement` FOREIGN KEY (`idModePaiement`) REFERENCES `modepaiement` (`idModePaiement`),
+  CONSTRAINT `fk_paiement_Ticket` FOREIGN KEY (`idTicket`) REFERENCES `ticket` (`idTicket`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paiement`
+--
+
+LOCK TABLES `paiement` WRITE;
+/*!40000 ALTER TABLE `paiement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paiement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produits`
+--
+
+DROP TABLE IF EXISTS `produits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produits` (
+  `idProduit` int(11) NOT NULL AUTO_INCREMENT,
+  `libelleProduit` varchar(50) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `dateDePeremption` date NOT NULL,
+  `idCategorie` int(11) NOT NULL,
+  PRIMARY KEY (`idProduit`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produits`
+--
+
+LOCK TABLES `produits` WRITE;
+/*!40000 ALTER TABLE `produits` DISABLE KEYS */;
+INSERT INTO `produits` VALUES (21,'p1',1,'2020-11-13',3);
+/*!40000 ALTER TABLE `produits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket` (
+  `idTicket` int(11) NOT NULL AUTO_INCREMENT,
+  `prixHT` float NOT NULL,
+  `date` date NOT NULL,
+  `montantTVA` float NOT NULL,
+  PRIMARY KEY (`idTicket`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tva`
+--
+
+DROP TABLE IF EXISTS `tva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tva` (
+  `idTva` int(11) NOT NULL AUTO_INCREMENT,
+  `tauxTva` float NOT NULL,
+  PRIMARY KEY (`idTva`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tva`
+--
+
+LOCK TABLES `tva` WRITE;
+/*!40000 ALTER TABLE `tva` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+  `identifiant` varchar(50) NOT NULL,
+  `motDePasse` varchar(50) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`idUser`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-07 17:00:01
